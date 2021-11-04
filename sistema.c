ace4a379232fe6fef10cc05398a92bbf3c9d958e
Porfirio2002 <pedrohenriquedasilva014@gmail.com>
2021-11-04 14:02:29 -0300

    04-11-2021-teladelogin

--- sistema.c
+++ sistema.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+typedef struct{
+    char login[30];
+    char senha[30];
+} pessoa; pessoa p[1];
+
 int main(){
+
+char login[30];
+    char senha[30];
+    strcpy(p[0].login, "admin");
+    strcpy(p[0].senha, "admin");
+
+    printf("\nlogin:");
+    scanf("%s", login);
+
+    printf("\nsenha:");
+    scanf("%s", senha);
+
+    if ((strcmp(login,p[0].login)==0) && (strcmp(senha,p[0].senha)==0)){
+        printf("Usuário logado");
+    }else{
+        printf("Login e/ou senha incorretos");
+    }
+    return 0;
+
+
     char Nome[45];
     char CPF[15];
     char Telefone[13];
